89c14d063c737c79cc05207bb71b517a630c4e40
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-10-24 23:46:26 -0300

    refactor: move class SignUpController to a factory method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController } from './signuo'
 import { MissingParamError } from '../errors/missing-param-error'
 
+const makeSut = () => {
+    return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
         body: {
         email: 'any_email@mail.com',
@@ -17,21 +21,21 @@ describe('SignUp Controller', () => {
     })
 
     test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
-    const httpRequest = {
-        body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
+        const sut = makeSut()
+        const httpRequest = {
+            body: {
+            name: 'any_name',
+            password: 'any_password',
+            passwordConfirmation: 'any_password'
+            }
         }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
 
     test('Should return 400 if no password is provided', () => {
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 name: 'any_name',
@@ -45,7 +49,7 @@ describe('SignUp Controller', () => {
     })
 
     test('Should return 400 if no passwordConfirmation is provided', () => {
-        const sut = new SignUpController()
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 name: 'any_name',
