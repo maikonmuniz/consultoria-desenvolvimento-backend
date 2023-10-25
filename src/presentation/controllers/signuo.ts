8fc7f8c483108daaefe582519bf2df05c88454b4
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-10-24 21:07:56 -0300

    feat: create class in controller signup

---
+++ src/presentation/controllers/signuo.ts
@@ -0,0 +1,14 @@
+import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+
+export class SignUpController {
+    handle(httpRequest: HttpRequest): HttpResponse {
+        const requiredFields = ['name', 'email', 'password']
+        for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field))
+            }
+        }
+    }
+}
