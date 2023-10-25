3d91b56937019b374dd4cbb5bb69fbff17714ddd
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-10-24 23:38:55 -0300

    feat: add file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
-/** @type {import('jest').Config} */
-const config = {
-  coverageProvider: "v8",
-};
-
-module.exports = config;
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
