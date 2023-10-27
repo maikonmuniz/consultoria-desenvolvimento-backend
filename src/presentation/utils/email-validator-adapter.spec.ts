ad117984aeabc0dff16b5760cf330f72b8a72943
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-10-27 18:40:56 -0300

    test: create factory EmailValidatorAdapter

--- src/presentation/utils/email-validator-adapter.spec.ts
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -8,23 +8,27 @@ jest.mock("validator", () => ({
     }
 }))
 
+const makeSut = () => {
+    return new EmailValidatorAdapter();
+}
+
 describe("EmailValidator Adapter", () => {
     test("Should return false if validator returns false", () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_email');
         expect(isValid).toBe(false);
     })
 
     test("Should return false if validator returns false", () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true);
         const isValid = sut.isValid('valid_email@email.com');
         expect(isValid).toBe(true);
     })
 
     test("Should call validator with correct email", () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         const isEmailSpy = jest.spyOn(validator, "isEmail");
         sut.isValid("any_email@email.com");        
         expect(isEmailSpy).toHaveBeenCalledWith("any_email@email.com");
