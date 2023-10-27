50325a1a3404418afd4d60c472958d60833a493e
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-10-27 18:04:24 -0300

    test: add jest mock in test adapters EmailValidator

--- src/presentation/utils/email-validator-adapter.spec.ts
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,25 @@
+import isEmail from "validator/lib/isEmail";
 import { EmailValidatorAdapter } from "./email-validator";
+import validator from "validator";
+
+jest.mock("validator", () => ({
+    isEmail(): boolean {
+        return true
+    }
+}))
 
 describe("EmailValidator Adapter", () => {
     test("Should return false if validator returns false", () => {
-        const sut = new EmailValidatorAdapter()
-        const isValid = sut.isValid('invalid_email@mail.com');
+        const sut = new EmailValidatorAdapter();
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+        const isValid = sut.isValid('invalid_email');
         expect(isValid).toBe(false);
+    })
+
+    test("Should return false if validator returns false", () => {
+        const sut = new EmailValidatorAdapter();
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true);
+        const isValid = sut.isValid('valid_email@email.com');
+        expect(isValid).toBe(true);
     })
 })
