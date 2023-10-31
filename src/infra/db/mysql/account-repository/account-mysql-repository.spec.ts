6acf1a43df4eec4ecc7a0e05fb6391e2ebc0f71d
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-10-30 23:52:18 -0300

    refactor: add disconnect

--- src/infra/db/mysql/account-repository/account-mysql-repository.spec.ts
+++ src/infra/db/mysql/account-repository/account-mysql-repository.spec.ts
@@ -4,7 +4,8 @@ import mysqlHelper from '../helpers/mysql-helper'
 describe('Bcrypt Adapter', () => {
     test('Should call bcrypt with correct values', async () => {
         await mysqlHelper.connect()
-        const consult = await mysqlHelper.consult();
+        const consult = await mysqlHelper.consult()
+        mysqlHelper.disconnect()
         expect(consult).toBeTruthy()
     })
 })
