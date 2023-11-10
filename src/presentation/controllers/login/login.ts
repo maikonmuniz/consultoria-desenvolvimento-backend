204289846a39d0af336b01a53c19a58998812194
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-10 20:54:58 -0300

    feact: create controller login

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,26 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { EmailValidator } from '../signup/signup-protocols'
+
+export class LoginController implements Controller {
+    private readonly emailValidator: EmailValidator
+
+    constructor(emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator
+    }
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const { email, password } = httpRequest.body
+        if (!email) {
+            return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+        }
+        if (!password) {
+            return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+        }
+        const isValid = this.emailValidator.isValid(email)
+        if (!isValid) {
+            return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+        }
+    }
+}
