8cca6dc0620f9c8968f83c00933db81f7096a455
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-10 15:44:00 -0300

    feat: create log repository

--- src/infra/db/mysql/log-repository/log.ts
+++ src/infra/db/mysql/log-repository/log.ts
@@ -1,6 +1,6 @@
 import { LogErrorRepository } from "../../../../data/protocols/log-error-repository";
 import mysqlHelper from "../helpers/mysql-helper"
- 
+
 export class LogMySQLRepository implements LogErrorRepository {
     async logError(stack: string): Promise<void> {
         const dataNow = new Date
