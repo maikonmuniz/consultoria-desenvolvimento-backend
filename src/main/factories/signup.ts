f35a8b29889e5ea379ac8983f3c6c2b3ae3fdaac
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-10 20:53:58 -0300

    refactor: correct path folder signup

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from "../../presentation/controllers/signuo";
+import { SignUpController } from "../../presentation/controllers/signup/signuo";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
